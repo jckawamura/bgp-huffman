859b35b3d4f01d8e88ec8d3f5d5e5b38ca976066
Jorge Kawamura <jorgeckw@gmail.com>
2018-07-09 17:41:00 -0300

    Testes com read_fwf

--- parse_bgp.py
+++ parse_bgp.py
@@ -4,11 +4,13 @@ pd.set_option("max_rows", 5)
 
 
 def read_bgp(file):
-    fields = ['Network', 'Next']
-    bgp = pd.read_csv(file, sep='\s+', skipinitialspace=True, usecols=fields)
+    # fields = ['Network', 'Next']
+    bgp = pd.read_fwf(file)
     bgp
     pass
 
 
 src = "files/amostra_bgp.txt"
 read_bgp(src)
+
+
