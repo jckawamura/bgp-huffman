463aa79f29d1ae3745ee619ab279783516422655
Jorge Kawamura <jorgeckw@gmail.com>
2018-07-10 00:53:56 -0300

    Adicionada função que pode ser útil futuramente

--- IPToNextHop.py
+++ IPToNextHop.py
@@ -9,3 +9,6 @@ class IPToNextHop(object):
     def __init__(self, network: ipaddress, next_hop: ipaddress):
         self.network = network
         self.next_hop = next_hop
+
+# TODO: adicionar este comando no algoritmo da trie
+print(ipaddress.ip_address('1.0.0.0') in ipaddress.ip_network('0.0.0.0/0'))
