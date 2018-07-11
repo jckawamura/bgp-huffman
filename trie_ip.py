ea37b656f996305ccd7fea0c813bf83b3d8d7631
Jorge Kawamura <jorgeckw@gmail.com>
2018-07-11 01:08:09 -0300

    Testes com a amostra da tabela BGP

--- trie_ip.py
+++ trie_ip.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 class TrieNode(object):
-    def __init__(self, char: str, label = None):
+    def __init__(self, char: str, label=None):
         self.char = char
         self.children = []
         self.word_finished = False
@@ -30,24 +30,26 @@ def add(root, word: str, label: int):
     node.word_finished = True
     node.label = label
 
+
 def leafpush(root):
-  if root.word_finished:
-    return True
-  else:
-    for child in root.children:
-      if child.char:
-        if not child.label:
-          child.label = root.label
-        leafpush(child)
-    if len(root.children)==1:
-      if root.children[0].char == '1':
-        new_node = TrieNode('0', root.label)
-      else:
-        new_node = TrieNode('1', root.label)
-      new_node.word_finished = True
-      root.children.append(new_node)
-      return True
-    root.label = None
+    if root.word_finished:
+        return True
+    else:
+        for child in root.children:
+            if child.char:
+                if not child.label:
+                    child.label = root.label
+                leafpush(child)
+        if len(root.children) == 1:
+            if root.children[0].char == '1':
+                new_node = TrieNode('0', root.label)
+            else:
+                new_node = TrieNode('1', root.label)
+            new_node.word_finished = True
+            root.children.append(new_node)
+            return True
+        root.label = None
+
 
 def find_prefix(root, prefix: str) -> Any:
     node = root
@@ -62,18 +64,28 @@ def find_prefix(root, prefix: str) -> Any:
 
 
 if __name__ == "__main__":
-    # src = "files/amostra_bgp.txt"
-    # print(parse_bgp.read_bgp(src, 1))
+    src = "files/amostra_bgp.txt"
+    bgp_table = bgp.read_bgp(src, 1)
+    ip_label_list = bgp.ip_next_hop(bgp_table)
 
-    root = TrieNode('*', 5)
+    root = TrieNode('*', 4)
 
-    add(root, "100", 4)
-    add(root, "0", 40)
-    add(root, "000", 43)
-    add(root, "0111", 42)
- 
+    for x in ip_label_list:
+        add(root, x[0], x[1])
 
+    # add(root, "100", 4)
+    # add(root, "0", 40)
+    # add(root, "000", 43)
+    # add(root, "0111", 42)
+    #
+    #
     leafpush(root)
 
-    print(find_prefix(root, "0"))
-    print(find_prefix(root, "001"))
+    print("Tabela BGP\n", bgp_table)
+    print("\nConversão da tabela BGP\n", ip_label_list)
+
+    print("\nTestes:\n")
+    print(find_prefix(root, "00000000000000000000000000000000"))
+    print(find_prefix(root, "00000001000000000000010000000000"))
+    print(find_prefix(root, "00000001000000000000010000000001"))
+    print(find_prefix(root, "00000001000000000000010000000010"))
