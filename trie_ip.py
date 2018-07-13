d47559cba5b02679fd1fc2321438b5f74569c288
Jorge Kawamura <jorgeckw@gmail.com>
2018-07-13 15:17:27 -0300

    Melhorando os comentários #2

--- trie_ip.py
+++ trie_ip.py
@@ -15,14 +15,10 @@ class TrieNode(object):
 
 
 # Adição de um elemento na trie
-# Step 1: separate the provided prefixes by protocol (IPv4 vs IPv6),
-# and build a Binary Trie per protocol.
-#
-# For example, if the input prefixes are
 #   A: 0.0.0.0/0
-#   B: 128.0.0.0/2  (10000000.0.0.0/2 in binary)
+#   B: 128.0.0.0/2  (10000000.0.0.0/2)
 #   C: 192.0.0.0/2  (11000000.0.0.0/2)
-# the Binary Trie for IPv4 will look like this at the end of step 1:
+#
 #          +---+
 #          | A |
 #          +---+
@@ -35,25 +31,22 @@ class TrieNode(object):
 #          | B |   | C |
 #          +---+   +---+
 #
-# Note that the prefixes in this example are nested: any IPv4 address
-# that matches B or C will also match A. Unfortunately, the classic LC Trie
-# algorithm does not support nested prefixes. The next step will solve that
-# problem.
-def add(root, word: str, label: int):
+def add(root, ip_prefix: str, label: int):
     node = root
-    if word == '':  # tratamento específico para a raiz da árvore.
+    if ip_prefix == '':  # tratamento específico para a raiz da árvore.
         root.label = label
-        # root.word_finished = True
         return
     # Devemos passar por cada elemento do IP binário, caminhando pela árvore para inserí-los nas posições corretas.
-    for char in word:
+    for char in ip_prefix:
         found_in_child = False
+        # Percorre a árvore enquando o prefixo ainda corresponde
         for child in node.children:
             if child.char == char:
                 node = child
                 found_in_child = True
                 node.word_finished = False
                 break
+        # Quando não encontra mais o valor inserido, é criado um novo nó
         if not found_in_child:
             new_node = TrieNode(char)
             node.children.append(new_node)
@@ -64,8 +57,6 @@ def add(root, word: str, label: int):
 
 # Faz leaf push de cada prefixo da árvore para as suas folhas
 #
-# Continuing the previous example, the Binary Trie will look like this
-# at the end of step 2:
 #          +---+
 #          |   |
 #          +---+
@@ -78,9 +69,6 @@ def add(root, word: str, label: int):
 #          | B |   | C |
 #          +---+   +---+
 #
-# This trie yields the same match results as the original trie from
-# step 1. But it has a useful new property: now that all the prefixes
-# are at the leaves, they are disjoint: no prefix is nested under another.
 def leaf_push(root):
     if root.word_finished:  # se já é nó folha, não há necessidade de fazer leaf push
         return
@@ -92,14 +80,14 @@ def leaf_push(root):
                 leaf_push(child)
         # Se um nó só tem um filho, ele deve criar um novo herdando o label.
         if len(root.children) == 1:
-            if root.children[0].char == '1':    # se já existe um filho do lado direito, surge um novo do lado esquerdo.
+            if root.children[0].char == '1':  # se já existe um filho do lado direito, surge um novo do lado esquerdo.
                 new_node = TrieNode('0', root.label)
-            else:   # se já existe um filho do lado esquerdo, surge um novo do lado direito.
+            else:  # se já existe um filho do lado esquerdo, surge um novo do lado direito.
                 new_node = TrieNode('1', root.label)
             new_node.word_finished = True
             root.children.append(new_node)
             return
-        # root.label = None
+        root.label = None
 
 
 # Dado um endereço IP, procura o prefixo com o maior matching e retorna o label de seu Next Hop
@@ -107,10 +95,11 @@ def find_prefix(root, prefix: str) -> Any:
     node = root
     if not root.children:
         return root.label
+    # Percorre a árvore até encontrar um nó folha.
     for char in prefix:
         for child in node.children:
             if child.char == char:
                 node = child
+                if child.word_finished:  # Se encontrar a folha, retorna o label associado.
+                    return child.label
                 break
-    return node.label
-
