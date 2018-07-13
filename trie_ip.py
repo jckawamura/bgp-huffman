f38bcbd1e6caca4773891551788b7369f16c2c92
Jorge Kawamura <jorgeckw@gmail.com>
2018-07-13 18:36:22 -0300

    Refactor

--- trie_ip.py
+++ trie_ip.py
@@ -7,10 +7,10 @@ from typing import Tuple, Any
 
 # Estrutura básica de um nó da trie
 class TrieNode(object):
-    def __init__(self, char: str, label=None):
-        self.char = char  # caractere binário do endereço IP
+    def __init__(self, bit_ip: str, label=None):
+        self.bit_ip = bit_ip  # caractere binário do endereço IP
         self.children = []  # lista de filhos do nó
-        self.word_finished = False  # indica se o nó é folha
+        self.is_leaf = False  # indica se o nó é folha
         self.label = label  # label do melhor Next Hop associado ao endereço procurado
 
 
@@ -37,21 +37,21 @@ def add(root, ip_prefix: str, label: int):
         root.label = label
         return
     # Devemos passar por cada elemento do IP binário, caminhando pela árvore para inserí-los nas posições corretas.
-    for char in ip_prefix:
+    for bit_ip in ip_prefix:
         found_in_child = False
         # Percorre a árvore enquando o prefixo ainda corresponde
         for child in node.children:
-            if child.char == char:
+            if child.bit_ip == bit_ip:
                 node = child
                 found_in_child = True
-                node.word_finished = False
+                node.is_leaf = False
                 break
         # Quando não encontra mais o valor inserido, é criado um novo nó
         if not found_in_child:
-            new_node = TrieNode(char)
+            new_node = TrieNode(bit_ip)
             node.children.append(new_node)
             node = new_node
-    node.word_finished = True
+    node.is_leaf = True
     node.label = label
 
 
@@ -70,21 +70,21 @@ def add(root, ip_prefix: str, label: int):
 #          +---+   +---+
 #
 def leaf_push(root):
-    if root.word_finished:  # se já é nó folha, não há necessidade de fazer leaf push
+    if root.is_leaf:  # se já é nó folha, não há necessidade de fazer leaf push
         return
     else:
         for child in root.children:
-            if child.char:
+            if child.bit_ip:
                 if not child.label:
                     child.label = root.label
                 leaf_push(child)
         # Se um nó só tem um filho, ele deve criar um novo herdando o label.
         if len(root.children) == 1:
-            if root.children[0].char == '1':  # se já existe um filho do lado direito, surge um novo do lado esquerdo.
+            if root.children[0].bit_ip == '1':  # se já existe um filho do lado direito, surge um novo do lado esquerdo.
                 new_node = TrieNode('0', root.label)
             else:  # se já existe um filho do lado esquerdo, surge um novo do lado direito.
                 new_node = TrieNode('1', root.label)
-            new_node.word_finished = True
+            new_node.is_leaf = True
             root.children.append(new_node)
             return
         root.label = None
@@ -96,10 +96,10 @@ def find_prefix(root, prefix: str) -> Any:
     if not root.children:
         return root.label
     # Percorre a árvore até encontrar um nó folha.
-    for char in prefix:
+    for bit_ip in prefix:
         for child in node.children:
-            if child.char == char:
+            if child.bit_ip == bit_ip:
                 node = child
-                if child.word_finished:  # Se encontrar a folha, retorna o label associado.
+                if child.is_leaf:  # Se encontrar a folha, retorna o label associado.
                     return child.label
                 break
